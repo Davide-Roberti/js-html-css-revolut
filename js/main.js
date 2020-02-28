bb5a1e8fbc5b39a74eaec812efa2c672448dd673
Davide <davide.roberti89@gmail.com>
2020-02-28 14:47:45 +0100

    inizio parte due

--- js/main.js
+++ js/main.js
@@ -1,26 +1,3 @@
 $(document).ready(function() {
-    // PRIMA PARTE SENZA class question
-    // $('h3').click(function() {
-    //     $(this).next('p').slideToggle();
-    // });
-    // Seconda parte con class question
-    $('.question').click(function(){
-
-        if($(this).children('p').is(':visible')) {
-            $(this).children('p').slideToggle(500);
-
-            $('.question').children('h3').removeClass('ruota');
-        } else {
-            $('.question').children('p').slideUp(500);
-            $(this).children('p').slideToggle(500);
-
-            $(this).children('h3').addClass('ruota');
-        }
-
-    });
-
-
-
-
-
+    
 });
