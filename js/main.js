79a7063f15bf95d5af11cd04615f89f3a592490e
Davide <davide.roberti89@gmail.com>
2020-02-28 15:25:50 +0100

    menu a tendina

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    
+    $('main').click(function(){
+        if($('.dropdown-menu').is(':visible')) {
+        $('.dropdown-menu').slideUp(300);
+        };
+    });
+    $('.menu-item').click(function(){
+        if($(this).children('.dropdown-menu').is(':visible')) {
+            $(this).children('.dropdown-menu').slideUp(300);
+        } else {
+            $('.menu-item').children('.dropdown-menu').slideUp(300);
+            $(this).children('.dropdown-menu').slideDown(300);
+        }
+    });
 });
